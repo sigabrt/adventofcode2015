59c301ec4123c8da107dc8b376926911fa00e62f
Alexander Meyer <alexander.dude.meyer@gmail.com>
2015-12-31 16:39:05 -0600

    Slightly cleaner version of day 4 part 2 (Clojure).

--- clojure/day04/day04part02.clj
+++ clojure/day04/day04part02.clj
@@ -3,10 +3,13 @@
 
 (require 'digest)
 
+(defn md5-with-postfix
+  [key val]
+  (digest/md5 (str key val)))
+
 (println
   (let [key (second *command-line-args*)]
-    (loop [val 1]
-      (let [hash (digest/md5 (str key val))]
-        (if (.startsWith hash "000000")
-          val
-          (recur (inc val)))))))
+    (first
+      (filter
+        #(.startsWith (md5-with-postfix key %1) "000000")
+        (range)))))
