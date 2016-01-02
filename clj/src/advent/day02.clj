6575f68d4bcecea8637adff5dc7f9792076b8be5
Alexander Meyer <alexander.dude.meyer@gmail.com>
2016-01-02 06:35:47 -0600

    Moved Clojure day 2 to new project.

---
+++ clj/src/advent/day02.clj
@@ -0,0 +1,37 @@
+(ns advent.day02
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn wrapping-paper-area
+  "Calculates the minimum amount of wrapping paper required for the given box."
+  [[l w h]]
+  (let [s1 (* l w)
+        s2 (* l h)
+        s3 (* w h)]
+    (+ (* s1 2) (* s2 2) (* s3 2) (min s1 s2 s3))))
+
+(defn wribbon-length
+  "Calculates the minimum amount of ribbon required to decorate the given box."
+  [[l w h]]
+  (let [p1 (+ (* l 2) (* w 2))
+        p2 (+ (* l 2) (* h 2))
+        p3 (+ (* w 2) (* h 2))]
+    (+ (min p1 p2 p3) (* l w h))))
+
+(defn part1
+  []
+  (reduce +
+    (map
+      (fn [line]
+        (let [dims (map read-string (str/split line #"x"))]
+          (wrapping-paper-area dims)))
+      (line-seq (java.io.BufferedReader. (io/reader (io/resource "day02.txt")))))))
+
+(defn part2
+  []
+  (reduce +
+    (map
+      (fn [line]
+        (let [dims (map read-string (str/split line #"x"))]
+          (wribbon-length dims)))
+      (line-seq (java.io.BufferedReader. (io/reader (io/file (io/resource "day02.txt"))))))))
