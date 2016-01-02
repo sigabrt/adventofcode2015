08737b936dd7be84eabbca2ea231a90c6289ff93
Alexander Meyer <alexander.dude.meyer@gmail.com>
2016-01-02 09:32:59 -0600

    Added dev profile to Clojure project.

---
+++ clj/dev/user.clj
@@ -0,0 +1,4 @@
+(ns user
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
--- clj/project.clj
+++ clj/project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/sigabrt/adventofcode2015"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.namespace "0.2.11"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
