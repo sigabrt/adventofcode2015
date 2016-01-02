362719aed69dafd66723b6616ae5dddd36e41908
Alexander Meyer <alexander.dude.meyer@gmail.com>
2016-01-02 10:51:39 -0600

    More imports for dev repl.

--- clj/dev/user.clj
+++ clj/dev/user.clj
@@ -1,4 +1,6 @@
 (ns user
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
+            [clojure.repl :refer :all]
+            [clojure.test :as test]
             [clojure.tools.namespace.repl :refer (refresh refresh-all)]))
