254b59e5fbea192c9d5ead280fdc1e65c77ec681
Alexander Meyer <alexander.dude.meyer@gmail.com>
2016-01-03 07:28:23 -0600

    Improved version of Clojure day 4.

--- clj/src/advent/day04.clj
+++ clj/src/advent/day04.clj
@@ -2,18 +2,21 @@
   (:require [clojure.java.io :as io]
             [digest]))
 
+(defn find-hash-with-prefix
+  [key prefix]
+  (first
+    (filter
+      #(.startsWith (digest/md5 (str key %1)) prefix)
+      (range))))
+
 (defn part1
   []
-  (let [key (slurp (io/resource "day04.txt"))]
-    (first
-      (filter
-        #(.startsWith (digest/md5 (str key %1)) "00000")
-        (range)))))
+  (find-hash-with-prefix
+    (slurp (io/resource "day04.txt"))
+    "00000"))
 
 (defn part2
   []
-  (let [key (slurp (io/resource "day04.txt"))]
-    (first
-      (filter
-        #(.startsWith (digest/md5 (str key %1)) "000000")
-        (range)))))
+  (find-hash-with-prefix
+    (slurp (io/resource "day04.txt"))
+    "000000"))
